dcfbeff17a748377eb6431d9df6f38fb21e09f3d
Karina <thealisky@gmail.com>
2024-11-24 00:53:33 +0300

    Initial commit

---
+++ src/main/java/com/example/t1_java_demo_app_2/T1JavaDemoApp2Application.java
@@ -0,0 +1,13 @@
+package com.example.t1_java_demo_app_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T1JavaDemoApp2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(T1JavaDemoApp2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/t1_java_demo_app_2/T1JavaDemoApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.t1_java_demo_app_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T1JavaDemoApp2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
